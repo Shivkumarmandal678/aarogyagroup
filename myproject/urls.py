8456149397f1817d0e5890c353ffc088fa7bf96c
Shiv Kumar <92754862+Shivkumarmandal678@users.noreply.github.com>
2026-08-24 12:37:31 +0545

    update

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include  # 'include' ko import karna zaroori hai
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        # Yahan humne myapp ke URLs ko link kar diya hai
-            path('', include('myapp.urls')), 
+    # Aapki app URLs yahan aayengi
+    path('', include('myapp.urls')), 
 ]
+# Media files setup for local development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
