643dcd0e23a2058ea18452f4cb7c4b4264c12b55
Shiv Kumar <92754862+Shivkumarmandal678@users.noreply.github.com>
2026-08-24 12:50:05 +0545

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media', # Media context processor
+                'django.template.context_processors.media',
             ],
         },
     },
